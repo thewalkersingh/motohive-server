ab70c04fbcdad5187ab16760956737d6280c017e
Diwakar <the.walkersingh@hotmail.com>
2026-06-11 17:20:21 +0530

    ✅ listing-service   — DONE (tested)

--- listing-service/src/main/java/com/motohive/listingservice/ListingServiceApplication.java
+++ listing-service/src/main/java/com/motohive/listingservice/ListingServiceApplication.java
@@ -10,10 +10,10 @@ public class ListingServiceApplication {
 	
 	public static void main(String[] args) {
 		SpringApplication.run(ListingServiceApplication.class, args);
-		printmsg();
+		start();
 	}
 	
-	static void printmsg() {
+	static void start() {
 		System.out.println("***************************************************************");
 		System.out.println("*****************-------------------------*********************");
 		System.out.println("****************| Listing-Service Started |********************");
