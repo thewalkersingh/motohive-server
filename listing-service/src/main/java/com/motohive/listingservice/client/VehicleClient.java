27096a9cd44f77b180c4a14e751c2987b57c5911
Diwakar <the.walkersingh@hotmail.com>
2026-06-11 16:26:24 +0530

    Feign Client

--- listing-service/src/main/java/com/motohive/listingservice/client/VehicleClient.java
+++ listing-service/src/main/java/com/motohive/listingservice/client/VehicleClient.java
@@ -1,5 +1,14 @@
 package com.motohive.listingservice.client;
 
-public class VehicleClient {
+import com.motohive.listingservice.dto.response.VehicleResponse;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
+@FeignClient(name = "vehicle-service", url = "${motohive.services.vehicle-service-url}")
+public interface VehicleClient {
+	
+	@GetMapping("/api/v1/vehicles/{id}")
+	VehicleResponse getVehicleById(@PathVariable("id") Long id);
+	
 }
--- vehicle-service/src/main/java/com/motohive/vehicleservice/VehicleServiceApplication.java
+++ vehicle-service/src/main/java/com/motohive/vehicleservice/VehicleServiceApplication.java
@@ -5,9 +5,19 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class VehicleServiceApplication {
-
+	
 	public static void main(String[] args) {
 		SpringApplication.run(VehicleServiceApplication.class, args);
+		printmsg();
 	}
-
+	
+	static void printmsg() {
+		System.out.println("***************************************************************");
+		System.out.println("*****************-------------------------*********************");
+		System.out.println("****************| Vehicle Service Started |********************");
+		System.out.println("*****************-------------------------*********************");
+		System.out.println("***************************************************************");
+		
+	}
+	
 }
